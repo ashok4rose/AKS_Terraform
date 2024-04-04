2818b9ecd447028c2a8b8d1e8402faf4b760f8dc
ashok4rose <165984115+ashok4rose@users.noreply.github.com>
2024-04-04 14:19:18 +0530

    modified variables.tf file

--- Variables.tf
+++ Variables.tf
@@ -1,17 +1,17 @@
 variable "subscription_id" {
-  default = "****************"
+  default = "5d8f6b29-f1f0-4bbe-bbf2-37f30d1cc5e9"
 }
 
 variable "client_id" {
-  default = "***************"
+  default = "7075042a-606e-4c84-a023-a9eb8d29c7c6"
 }
 
 variable "client_secret" {
-  default = "**************"
+  default = " "J5w8Q~rgH-J1oD30Lui9Fey4BpN6qxoBjz8L9dpH"
 }
 
 variable "tenant_id" {
-  default = "************"
+  default = "0514457c-107a-4b0d-bcd2-488d71696eef"
 }
 
 variable "resource_group_name" {
